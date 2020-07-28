741bb87c3dc56ddba7e187904656e49d418f413f
Anna Teng <anna.w.teng@gmail.com>
2020-07-28 13:22:15 -0400

    fixed scrolling

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,8 +1,20 @@
 import React from 'react'
-import { images } from 'Utilities/common'
 
 const Footer = () => (
-  <div className="footer">
+  <div className="footer" >
+  <div style={{ width: '100%', maxWidth: '1200px', display: 'flex', flexDirection: 'row' }} className='centered-div'>
+    <div style={{ width: '33%'}}>
+    <div>Created by Anna Teng</div> 
+    <a href='https://github.com/annateng'>https://github.com/annateng</a>
+    </div>
+    <div style={{ width: '33%', display: 'flex', alignItems: 'bottom', flexDirection: 'column' }}>
+    <div>Contact me:</div>
+    <div>(781) 789-6616</div>
+    </div>
+    <div style={{ height: '100%', width: '33%', display: 'flex', alignItems: 'bottom', flexDirection: 'column' }}>
+    <div><a href='mailto:anna.w.teng@gmail.com?subject=[Skidoodle]'>anna.w.teng@gmail.com</a></div>
+    </div>
+    </div>
   </div>
 )
 
